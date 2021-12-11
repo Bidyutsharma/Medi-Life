3b8daf1d676376e4f91d76a3ef4adc6b58935c78
Bidyutsharma <sudiptosharma2@gmail.com>
2021-12-12 04:23:28 +0600

    update-medichine

--- app/src/main/java/com/example/signup/medicine.java
+++ app/src/main/java/com/example/signup/medicine.java
@@ -2,13 +2,26 @@ package com.example.signup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class medicine extends AppCompatActivity {
-
+    Button aceBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medicine);
+        aceBtn = (Button) findViewById(R.id.aceBtn);
+        aceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(medicine.this,"Item Clicked",Toast.LENGTH_SHORT).show();
+                Intent intentMedi = new Intent(medicine.this,medicineDetails.class);
+                startActivity(intentMedi);
+            }
+        });
     }
 }
